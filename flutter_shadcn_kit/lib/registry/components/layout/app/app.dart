2b179e5ba02ae9b1c08727c9e836d32c575d8403
M Abrar Javed <abrar.javed3@gmail.com>
2026-05-18 01:46:31 +0100

    Make default app bootstrap lean

--- flutter_shadcn_kit/lib/registry/components/layout/app/app.dart
+++ flutter_shadcn_kit/lib/registry/components/layout/app/app.dart
@@ -1,8 +1,5 @@
 // ignore_for_file: duplicate_import, unnecessary_import, unused_import, unnecessary_null_comparison, dead_code, deprecated_member_use, use_null_aware_elements, sort_child_properties_last
 
-export '../../display/divider/divider.dart' show Divider;
-export '../scaffold/scaffold.dart' show AppBar, Scaffold;
-
 import 'package:flutter/material.dart' as material;
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter/widgets.dart';
